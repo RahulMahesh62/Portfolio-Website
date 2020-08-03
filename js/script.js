f6e23b52dbb71fcb07b3f8e6e173d10e28c99123
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-08-03 17:41:04 +0530

    Added scroll to top action

--- js/script.js
+++ js/script.js
@@ -17,20 +17,6 @@ inputs.forEach((input) => {
     input.addEventListener("blur", blurFunc);
 });
 
-mybutton = document.getElementById("fcircle");
-
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 0) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function scrollToTop() {
+    $(window).scrollTop(0);
 }
