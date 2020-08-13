e2c1ac75d5b2b217e7eb1ec0e1461b3b4982435d
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-08-14 00:05:52 +0530

    Minified JS

--- js/script.js
+++ js/script.js
@@ -1,83 +1,11 @@
-$(document).ready(function() {
-    //Preloader
-    preloaderFadeOutTime = 350;
-
-    function hidePreloader() {
-        var preloader = $('.spinner-wrapper');
-        preloader.fadeOut(preloaderFadeOutTime);
-    }
-    hidePreloader();
-});
-
+$(document).ready(function() { preloaderFadeOutTime = 350, $(".spinner-wrapper").fadeOut(preloaderFadeOutTime) });
 const inputs = document.querySelectorAll(".input");
 
-function focusFunc() {
-    let parent = this.parentNode;
-    parent.classList.add("focus");
-}
+function focusFunc() { this.parentNode.classList.add("focus") }
 
-function blurFunc() {
-    let parent = this.parentNode;
-    if (this.value == "") {
-        parent.classList.remove("focus");
-    }
-}
+function blurFunc() { let t = this.parentNode; "" == this.value && t.classList.remove("focus") }
 
-inputs.forEach((input) => {
-    input.addEventListener("focus", focusFunc);
-    input.addEventListener("blur", blurFunc);
-});
+function scrollToTop() { $(window).scrollTop(0) }
 
-function scrollToTop() {
-    $(window).scrollTop(0);
-}
-
-$(document).ready(function() {
-    $(".sbtn").click(function() {
-        var name = $(this).attr("data-filter");
-        if (name == "all") {
-            $(".filter").show("2000");
-        } else {
-            $(".filter").not("." + name).hide("2000");
-            $(".filter").filter("." + name).show("2000");
-        }
-    });
-    $(".snav a").click(function() {
-        $(this).addClass("actv").siblings().removeClass("actv");
-    });
-});
-
-document.querySelector('.toggle').addEventListener('click', function() {
-    this.classList.add('animate');
-
-    setTimeout(() => {
-        this.classList.toggle('active');
-        document.querySelector('.wave').classList.toggle('active');
-        document.documentElement.classList.toggle('theme-dark');
-    }, 150);
-
-    setTimeout(() => this.classList.remove('animate'), 300);
-});
-
-function menuToggle() {
-    var mnav = document.getElementById('menu-overlay');
-    mnav.classList.toggle('actva');
-    var mnav = document.getElementById('toggle-icon');
-    mnav.classList.toggle('actva');
-}
-
-
-$(document).ready(function() {
-    $(".tech-grid").hide();
-    $(".hidebtn").hide();
-    $(".showbtn").click(function() {
-        $(".showbtn").hide();
-        $(".hidebtn").show();
-        $(".tech-grid").show();
-    });
-    $(".hidebtn").click(function() {
-        $(".showbtn").show();
-        $(".hidebtn").hide();
-        $(".tech-grid").hide();
-    });
-});
+function menuToggle() { document.getElementById("menu-overlay").classList.toggle("actva"), document.getElementById("toggle-icon").classList.toggle("actva") }
+inputs.forEach(t => { t.addEventListener("focus", focusFunc), t.addEventListener("blur", blurFunc) }), $(document).ready(function() { $(".sbtn").click(function() { var t = $(this).attr("data-filter"); "all" == t ? $(".filter").show("2000") : ($(".filter").not("." + t).hide("2000"), $(".filter").filter("." + t).show("2000")) }), $(".snav a").click(function() { $(this).addClass("actv").siblings().removeClass("actv") }) }), document.querySelector(".toggle").addEventListener("click", function() { this.classList.add("animate"), setTimeout(() => { this.classList.toggle("active"), document.querySelector(".wave").classList.toggle("active"), document.documentElement.classList.toggle("theme-dark") }, 150), setTimeout(() => this.classList.remove("animate"), 300) }), $(document).ready(function() { $(".tech-grid").hide(), $(".hidebtn").hide(), $(".showbtn").click(function() { $(".showbtn").hide(), $(".hidebtn").show(), $(".tech-grid").show() }), $(".hidebtn").click(function() { $(".showbtn").show(), $(".hidebtn").hide(), $(".tech-grid").hide() }) });
