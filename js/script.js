a51820a7e2b9f7005b5058b4018d8275326a8239
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-08-14 19:15:24 +0530

    Some minor tweaks

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ function scrollToTop() { $(window).scrollTop(0) }
 
 function menuToggle() { document.getElementById("menu-overlay").classList.toggle("actva"), document.getElementById("toggle-icon").classList.toggle("actva") }
 inputs.forEach(t => { t.addEventListener("focus", focusFunc), t.addEventListener("blur", blurFunc) }), $(document).ready(function() { $(".sbtn").click(function() { var t = $(this).attr("data-filter"); "all" == t ? $(".filter").show("2000") : ($(".filter").not("." + t).hide("2000"), $(".filter").filter("." + t).show("2000")) }), $(".snav a").click(function() { $(this).addClass("actv").siblings().removeClass("actv") }) }), document.querySelector(".toggle").addEventListener("click", function() { this.classList.add("animate"), setTimeout(() => { this.classList.toggle("active"), document.querySelector(".wave").classList.toggle("active"), document.documentElement.classList.toggle("theme-dark") }, 150), setTimeout(() => this.classList.remove("animate"), 300) }), $(document).ready(function() { $(".tech-grid").hide(), $(".hidebtn").hide(), $(".showbtn").click(function() { $(".showbtn").hide(), $(".hidebtn").show(), $(".tech-grid").show() }), $(".hidebtn").click(function() { $(".showbtn").show(), $(".hidebtn").hide(), $(".tech-grid").hide() }) });
+
+$('#toggle-icon').click(function() {
+    $(this).toggleClass('open');
+});
+
+
+$('#menu-overlay li').on('click', function() {
+    $("#menu-overlay").hide();
+    $("#toggle-icon").removeClass("open");
+    // $("#toggle-icon").addClass("open");
+    $('#menu-overlay').toggleClass('actva').show();
+});
